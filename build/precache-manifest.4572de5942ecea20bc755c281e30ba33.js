c78e99ff3a2272c2ef7a2609f52f69797aa72855
Eman Bau <emanbautista410@gmail.com>
2020-09-25 08:48:32 -0400

    add files

---
+++ build/precache-manifest.4572de5942ecea20bc755c281e30ba33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd6ac194ff47fcc2605c27e96cb5338",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a20d22178f34414dbf6",
+    "url": "/static/css/main.b8034654.chunk.css"
+  },
+  {
+    "revision": "6f626b51ba606b9c6908",
+    "url": "/static/js/2.20ed48d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.20ed48d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a20d22178f34414dbf6",
+    "url": "/static/js/main.2726b6b8.chunk.js"
+  },
+  {
+    "revision": "7975832572c639b7c3ee",
+    "url": "/static/js/runtime-main.a31525f6.js"
+  }
+]);
---
+++ build/static/js/main.2726b6b8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpemanbau-website-react"]=this["webpackJsonpemanbau-website-react"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(2),s=n.n(c);n(8);var u=function(){return r.a.createElement("div",{className:"App"})};s.a.render(r.a.createElement(u,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.2726b6b8.chunk.js.map
