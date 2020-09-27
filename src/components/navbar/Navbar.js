e1cc09ef53c9f965bc1591902cc21ec94f7a6c7d
Eman Bau <emanbautista410@gmail.com>
2020-09-26 20:22:05 -0400

    added files

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+import logo from '../../img/logo.png'
 
 function Navbar() {
     return (
         <div>
-            
+            <Router>
+                <nav className="navbar">
+                    <div className="navbar-container">
+                        <Link to='/' className='navbar-logo'>
+                            <img src={logo} alt="logo" className='navbar-logo-emanbau' />
+                        </Link>
+                        <div className="navbar-menu">
+                            
+                        </div>
+                    </div>
+                </nav>
+            </Router>
         </div>
     )
 }
