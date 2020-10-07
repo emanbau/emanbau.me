36a20eae94db2c8f7a46d23d9d4d1c6f9d5a7be1
Eman Bau <emanbautista410@gmail.com>
2020-10-06 21:29:46 -0400

    added text carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import VantaTest from '../components/VantaTest'
 import './Home.css'
+import TypedComponent from '../components/TypedComponent';
 
 function Home() {
+
+
     return (
+
         <div className='home-container'>
             <VantaTest />
             <div className='home-text-container'>
-                
+                <TypedComponent strings={[
+                    'Developer.',
+                    'Designer.',
+                    'Student.'
+                ]}
+                />
             </div>
         </div>
     )
