bb2f3b1e2693d68155182f915027ae429deba507
Eman Bau <emanbautista410@gmail.com>
2021-04-11 16:54:02 -0400

    Redo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import Projects from './pages/Projects';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Home />
-        <Switch>
-          <Route path='/' />
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/contact'>
-            <Contact />
-          </Route>
-          <Route path='/portfolio'>
-            <Portfolio />
-          </Route>
-          <Route path='/projects'>
-            <Projects />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,65 @@
 import React from 'react'
-import VantaTest from '../components/VantaTest'
-import './Home.css'
-import TypedComponent from '../components/TypedComponent';
+import { useRef, useEffect } from 'react';
+import './Home.css';
+import logo from '../assets/Eman Logo@3x.png'
+import { StyleSheet, css } from 'aphrodite';
+import Typed from 'typed.js';
 
 function Home() {
+    const typedComponentRef = useRef()
 
+    useEffect(() => {
+        const options = {
+            strings: ['ui/ux designer', 'web developer', 'node.js hero', 'certified lover boy'],
+            typeSpeed: 100,
+            backSpeed: 100,
+            loop: true,
+            loopCount: Infinity,
+            showCursor: true,
+            startDelay: 500,
+            backDelay: 1500,
+            smartBackspace: false,
+        }
+
+        new Typed(typedComponentRef.current, options)
+    }, [typedComponentRef])
 
     return (
-
-        <div className='home-container'>
-            <div className='home-text-container'>
-                <TypedComponent className='typed-container' strings={[
-                    'Developer.',
-                    'Designer.',
-                    'Student.'
-                ]}
-                />
+        <div className='landing-page-container'>
+            <img className={css(styles.logo)} src={logo} alt='logo'/>
+            <div className={css(styles.landingTextContainer)}>
+                <h1 className='heading'>Hey, I'm <span className='bold'>Eman</span>.</h1>
+                <p className='paragraph'>I’m a <span className='bold' ref={typedComponentRef} /> <br/> focusing on helping others build <br/> <span className="bold">meaningful</span> experiences. </p>
             </div>
-            <VantaTest />
         </div>
     )
 }
 
 export default Home
+
+// Aphrodite Styles
+const styles = StyleSheet.create({
+    logo: {
+        position: 'absolute',
+        left: '10vw',
+        top: '10vh',
+        maxWidth: '60px',
+        height: 'auto',
+    },
+
+    landingTextContainer: {
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        '@media screen and (min-width: 992px)': {
+            marginLeft: '25vw',
+        },
+        '@media screen and (max-width: 991px) and (min-width: 766px)': {
+            marginLeft: '13vw',
+        },
+        '@media screen and (max-width: 767px)': {
+            marginLeft: '0vw',
+        },
+    },
+})
