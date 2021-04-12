c35afb073387e01090c4b4288f341ea67f33b92b
Eman Bau <emanbautista410@gmail.com>
2021-04-12 19:01:29 -0400

    About Me

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useRef, useEffect } from 'react';
 import './Home.css';
-import logo from '../assets/Eman Logo@3x.png'
+import logo from '../assets/Eman Logo@3x.png';
+import eman from '../assets/emanphoto.jpg';
 import { StyleSheet, css } from 'aphrodite';
 import Typed from 'typed.js';
 
@@ -25,12 +26,40 @@ function Home() {
     }, [typedComponentRef])
 
     return (
-        <div className='landing-page-container'>
-            <img className={css(styles.logo)} src={logo} alt='logo'/>
-            <div className={css(styles.landingTextContainer)}>
-                <h1 className='heading'>Hey, I'm <span className='bold'>Eman</span>.</h1>
-                <p className='paragraph'>I’m a <span className='bold' ref={typedComponentRef} /> <br/> focusing on helping others build <br/> <span className="bold">meaningful</span> experiences. </p>
-                <a href='https://www.upwork.com/freelancers/~0144c5c1dc348b1f32' target="_blank" rel="noreferrer noopener" className='button'>Hire Me!</a>
+        <div className="home-page">
+            <div className='landing-page-container'>
+                <img className={css(styles.logo)} src={logo} alt='logo'/>
+                <div className={css(styles.landingTextContainer)}>
+                    <h1 className='heading'>Hey, I'm <span className='bold'>Eman</span>.</h1>
+                    <p className='paragraph'>I’m a <span className='bold' ref={typedComponentRef} /> <br/> focusing on helping others build <br/> <span className="bold">meaningful</span> experiences. </p>
+                    <a href='https://www.upwork.com/freelancers/~0144c5c1dc348b1f32' target="_blank" rel="noreferrer noopener" className='button'>Hire Me!</a>
+                </div>
+                <div className={css(styles.nav)}>
+                    <div className='item1'>
+                        <a className='nav-title'>Work</a>
+                        <div className='bars'/>
+                    </div>
+                    <div className='item2'>
+                        <a href='https://www.upwork.com/freelancers/~0144c5c1dc348b1f32' target="_blank" rel="noreferrer noopener" className='nav-title'>Contact</a>
+                        <div className='bars' />
+                    </div>
+                </div>
+            </div>
+            <div className="about-me">
+                <div className="about-me-content-container">
+                    <img src={eman} alt="" className="eman-photo"/>
+                    <div className="about-me-text-container">
+                        <h1 className='about-me-heading'>About Me</h1>
+                        <div className='about-me-paragraph'>
+                            Currently, I work as a <span className="bold2">freelance web developer and ui/ux designer</span> to help clients
+                            bring their ideas to life. I am also working on developing a free to use translation app
+                            called <span className="bold2">TranslateSmart</span> with react-native, as well as creating and managing a 
+                            lofi/chillhop/jazzhop radio called <span className="bold2">The Bap Café</span>. I am currently a student at the
+                            <span className="bold2"> University of Maryland</span> studying <span className="bold2">Computer Science</span>.
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
@@ -63,4 +92,19 @@ const styles = StyleSheet.create({
             marginLeft: '0vw',
         },
     },
+
+    nav: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        minHeight: 'auto',
+        minWidth: 'auto',
+        right: '0%',
+        marginRight: '30px',
+        '@media screen and (max-width: 767px)': {
+            display: 'none',
+        },
+    },
+
 })
